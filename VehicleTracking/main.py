6c6bde6fb4d67a012212fcf8138aabb757efbdd5
toanpn <toanphamhsgs@gmail.com>
2020-11-17 08:06:30 +0700

    save changes

--- VehicleTracking/main.py
+++ VehicleTracking/main.py
@@ -15,65 +15,59 @@ SIGMA_X = 4
 SIGMA_Y = 4
 
 
-# cap = cv2.VideoCapture("E:\\LUAN VAN\\repo\\VIDEO\\bike_counter_10min.mp4")
-cap = cv2.VideoCapture(0)
-threshold = 10
+cap = cv2.VideoCapture("E:\\LUAN VAN\\repo\VIDEO\\traffic_no_shadow\\20170812_162710.mp4")
+# cap = cv2.VideoCapture(0)
+threshold = 30
 preframe = None
 index = 0
 A = np.full((480, 640), 0)
 while(cap.isOpened()):
     ret, frame = cap.read() 
     # cv2.imshow('frame',frame)
-    frameGauss = cv2.GaussianBlur(frame, ksize=(KERNEL_WIDTH, KERNEL_HEIGHT), sigmaX=SIGMA_X, sigmaY=SIGMA_Y)
     height, width, channels = frame.shape
-    gray = cv2.cvtColor(frameGauss, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('a',gray)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, ksize=(KERNEL_WIDTH, KERNEL_HEIGHT), sigmaX=SIGMA_X, sigmaY=SIGMA_Y)
     if index != 0:
-        # temp = gray - preframe
         temp = cv2.absdiff(preframe, gray)
         # cv2.imshow('aav v',temp)
         ret,thresh1 = cv2.threshold(temp,threshold,255,cv2.THRESH_BINARY)
         # thresh1 = cv2.adaptiveThreshold(temp,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
         #     cv2.THRESH_BINARY_INV,11,2)
 
-        
-
+        kernel = np.ones((30,30),np.uint8)
         #
-        thresh1 = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (31,31)))
-
+        thresh1 = cv2.morphologyEx(thresh1, cv2.MORPH_CLOSE, kernel)
         # Find contours in thresh_gray after closing the gaps
-        # image, contours, hier = cv2.findContours(thresh1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        
-        for c in contours:
-            area = cv2.contourArea(c)
+        contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Find the convex hull object for each contour
+        hull_list = []
+        for i in range(len(contours)):
+            hull = cv2.convexHull(contours[i])
+            hull_list.append(hull)
+        # Draw contours + hull results
+        drawing = np.zeros((thresh1.shape[0], thresh1.shape[1], 3), dtype=np.uint8)
+        color = (255,255,255)
+        for i in range(len(contours)):
+            cv2.drawContours(drawing, contours, i, color,cv2.FILLED,cv2.MARKER_SQUARE)
+            cv2.drawContours(drawing, hull_list, i, color,cv2.FILLED,cv2.MARKER_SQUARE)
+            
+        for i in range(len(hull_list)):
+            M = cv2.moments(hull_list[i])
+            if M['m00'] != 0 and M['m00'] != 0:
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                cv2.circle(drawing, (cx,cy), radius=5, color=(0, 255, 0), thickness=-1)
+        # for c in contours:
+        #     area = cv2.contourArea(c)
 
-            # Small contours are ignored.
-            if area < 500:
-                cv2.fillPoly(thresh1, pts=[c], color=0)
-                continue
+        #     # Small contours are ignored.
+        #     if area < 50:
+        #         cv2.fillPoly(thresh1, pts=[c], color=0)
+        #         continue
 
-            # rect = cv2.minAreaRect(c)
-            # box = cv2.boxPoints(rect)
-            # convert all coordinates floating point values to int
-            # box = np.int0(box)
-            # cv2.drawContours(frame, [box], 0, (176, 39, 158),1)
-            # eclipse
-            ellipse = cv2.fitEllipse(c)
-            cv2.ellipse(frame,ellipse,(0,255,0),2)
-            #cỉcle
-            # (x,y),radius = cv2.minEnclosingCircle(c)
-            # center = (int(x),int(y))
-            # radius = int(radius)
-            # cv2.circle(frame,center,radius,(0,255,0),2)
-
-
-        #
-        #
-        #
-        # cv2.drawContours(frame, contours, -1, (176, 39, 158), 2)
-
-        cv2.imshow('temp',temp)
+        #     ellipse = cv2.fitEllipse(c)
+        #     cv2.ellipse(frame,ellipse,(0,255,0),2)
+        cv2.imshow('drawing',drawing)
         cv2.imshow('thresh1',thresh1)
         cv2.imshow('a',frame)
     preframe = gray
---
+++ VehicleTracking/main2.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2
+import time
+from PIL import Image
+
+cap = cv2.VideoCapture("E:\\LUAN VAN\\repo\\VIDEO\\traffic_no_shadow\\20170812_162903.MP4")
+threshold = 20
+preFrameGray = None
+preFrameGray2 = None
+preFrameGray3 = None
+index = 0
+A = np.full((480, 640), 0)
+
+KERNEL_WIDTH = 9
+KERNEL_HEIGHT = 9
+SIGMA_X = 4
+SIGMA_Y = 4
+
+while(cap.isOpened()):
+    ret, thisFrame = cap.read() 
+
+    height, width, channels = thisFrame.shape
+    #change frame to gray
+    thisFrameGray = cv2.cvtColor(thisFrame, cv2.COLOR_BGR2GRAY)
+    thisFrameGray_gauss = cv2.GaussianBlur(thisFrameGray, ksize=(KERNEL_WIDTH, KERNEL_HEIGHT), sigmaX=SIGMA_X, sigmaY=SIGMA_Y)
+
+    if index != 0:
+        temp_gauss = cv2.absdiff(preFrameGray2, thisFrameGray_gauss)
+        ret,thresh1 = cv2.threshold(temp_gauss,threshold,255,cv2.THRESH_BINARY)
+        contours, hierarchy = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        drawing = np.zeros((thresh1.shape[0], thresh1.shape[1], 3), dtype=np.uint8)
+        color = (255,255,255)
+        frameContours = cv2.drawContours(drawing, contours, -1, (0,255,0), -1)
+        hull_list = []
+        for i in range(len(contours)):
+            hull = cv2.convexHull(contours[i])
+            hull_list.append(hull)
+        frameHull = cv2.drawContours(drawing, hull_list, -1, (0,255,0), -1)
+        cv2.imshow('frame',thisFrame)
+        cv2.imshow('temp_gauss',temp_gauss)
+        cv2.imshow('thresh1',thresh1)
+        cv2.imshow('frameContours',frameContours)
+        cv2.imshow('frameHull',frameHull)
+    preFrameGray2 = thisFrameGray_gauss
+    index += 1
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
